0ca379ede597035120a8af13d0dab1ad77657547
yousefshref <yb2005at@gmail.com>
2023-10-16 07:48:54 +0300

    daa

--- server/settings.py
+++ server/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:IXB707K4r6zto83n19kC@containers-us-west-144.railway.app:6194/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'IXB707K4r6zto83n19kC',
+        'HOST': 'containers-us-west-144.railway.app',
+        'PORT': 6194,
     }
 }
 
